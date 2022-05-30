e6ce786a0936c19cefbbbcbc6b7b2e3d215867c5
prithivi10d <prithivi@10decoders.in>
2022-05-30 12:50:13 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit{
+  todoArray: any = [];
+  index:number = 0;
+  todoForm:any  =  FormGroup;
+  completedtask: boolean = false;
+  constructor(private formBuilder: FormBuilder) {
+
+    this.todoForm = this.formBuilder.group({
+        taskName: ['']
+    })
+  }
+  ngOnInit(): void {
+  }
+
+  get taskName() {
+    return this.todoForm.get('taskName');
+  }
+  addArray(): void{
+      this.todoArray[this.index] = this.todoForm.value;
+      console.log(this.todoArray)
+      this.todoForm.reset();
+      this.index = this.index + 1;
+  }
+  edit(item: any , i: number) :void {
+    this.index = i;
+    this.todoForm.setValue({
+      taskName: item.taskName
+    })
+  }
+
+  completed(): void {
+    this.completedtask = true;
+  }
+  delete(index: any):void {
+    this.todoArray.splice(index,1);
+  }
 }
